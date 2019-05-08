86365df71d429b27e4e415381bbd38bf02bc224c
Courtney Fink <courtney55@knights.ucf.edu>
2019-05-08 19:44:51 +0000

    added geocoder

--- Gemfile
+++ Gemfile
@@ -72,6 +72,8 @@ end
 gem 'will_paginate', '> 3.0'
 gem 'font-awesome-rails'
 gem 'devise'
+gem 'geocoder'
+gem 'figaro'
 
 
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode 
+
+  validates :name, presence: true, length: { minimum: 3, too_short: "must be at least %{count} characters long" }
+  validates :address, presence: true
+  validates :description, presence: true
 end
