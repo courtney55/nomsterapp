853d00d31048d5028f04946275a200f933b6d5c7
Courtney Fink <courtney55@knights.ucf.edu>
2019-05-08 16:36:50 +0000

    added detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 1)
+    @places = Place.all.paginate(:page => params[:page], :per_page => 1)
   end
 
   def new
@@ -12,6 +12,10 @@ class PlacesController < ApplicationController
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
