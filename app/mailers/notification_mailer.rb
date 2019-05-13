17b28317c552b3f075c267fbdc441545bc9bcd34
Courtney Fink <courtney55@knights.ucf.edu>
2019-05-13 18:54:14 +0000

    added notification mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@sanaapp.com"
+
+  def comment_added
+    mail(to: "courtney55@knights.ucf.edu",
+      subject: "A comment has been added to your place")
+  end
+
+end
